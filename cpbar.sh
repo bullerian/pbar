036ae1418ac5319c4fac6f60cc56d88447ad3e3b
Dmytro <lizometrix@gmail.com>
2017-05-29 11:36:56 +0300

    bc calc replaced with smarter option; progressbar func truncates values larger than 100 to 100

--- cpbar.sh
+++ cpbar.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 src=$1
 dst=$2
@@ -10,7 +10,7 @@ progress=0
 temp=0
 declare -A sizesOfFiles
 
-#Genereates test file
+#Generates test file
 #dd if=/dev/zero of=source count=400000 bs=1000 iflag=fullblock
 
 progressbar(){
@@ -18,12 +18,15 @@ progressbar(){
   readonly local BAR_START=$'\033[1m[\033[32m'
   readonly local BAR_END=$'\033[0m]\033[0m'
   local progCharCount
+  local percent=$1
 
-  if [[ $1 -le "100" && $1 -ge "0" ]]; then
-    progCharCount=$((($1*$BAR_TOTAL_CHAR)/100))
+  if [[ $percent -gt '100' ]]; then
+    percent=100
+  fi
+  
+    progCharCount=$((($percent*$BAR_TOTAL_CHAR)/100))
     string=$BAR_START$(printf '%*s' $progCharCount | tr ' ' $barChar)$(printf '%*s' $((BAR_TOTAL_CHAR - progCharCount)))$BAR_END
     echo "$string"
-  fi
 }
 
 #Check if both args are folders
@@ -38,7 +41,10 @@ fileList=$( find $src -maxdepth 1 -type f )
 #associate files with their sizes
 #calc total size of files in kb
 for i in $fileList; do
+  # unquoted echo removes tab symbols
+  # so size can be properly extracted
   tmp=$(echo $(du "$i"))
+  #the drawback of hash list is that it is unordered
   sizesOfFiles["$i"]=${tmp%% *}
   ((totalSize+=${sizesOfFiles["$i"]}))
 done
@@ -47,22 +53,26 @@ done
 fileCount=${#sizesOfFiles[@]}
 
 printf '\n'
+#save cursor location
 tput sc
 
 #main loop
+progress=0
 for srcFile in "${!sizesOfFiles[@]}"; do
   cp "$srcFile" "$dst"
 
   if [ $? -eq 0 ]; then
-    currProgress=0
-    currProgress=$(bc -l <<< "scale=2; ${sizesOfFiles[$srcFile]} / $totalSize * 100")
-    ((progress+= ${currProgress/.*/''}))
+    #smart way of calculating percentage
+    tmp=$((200*${sizesOfFiles[$srcFile]}/$totalSize % 2 + 100*${sizesOfFiles[$srcFile]}/$totalSize))
+    ((progress+=tmp))
+    #echo $progress
     ((temp++))
+    #echo $temp
 
     tput el1
     tput rc
     tput cuu1
-
+    #echo "ok"
     printf '%s %s%% %u of %u\nFile: %s' "$(progressbar $progress)" "$progress" "$temp" "$fileCount" "${srcFile/#*'/'/''}"
   else
     echo "error"
